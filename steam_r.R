edb0ac3f906c8b616cf2515bc55b5c5b8868e893
JonathonMifsud <jonathon.mifsud1@students.mq.edu.au>
2019-07-30 21:07:03 +1000

    started tidytuesday

---
+++ steam_r.R
@@ -0,0 +1,18 @@
+## TidyTuesday 30/7/19
+## Steam Game data
+
+library(tidyverse)
+library(tidytext)
+
+# load in data
+video_games <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-30/video_games.csv")
+
+
+title_words <- video_games %>%
+  filter(!str_detect(game, "[:digit:]")) %>%
+  select(game, owners) %>% 
+  unnest_tokens(word, game) %>%
+  anti_join(stop_words, by = "word") %>% 
+  count(word, sort = TRUE)
+  
+  
